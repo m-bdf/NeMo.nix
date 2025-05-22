8e1509e487fb5b5684bf859b3979f432c05582f3
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-05-22 06:32:32 +0200

    Add source hashes prefetcher

--- default.nix
+++ default.nix
@@ -5,7 +5,11 @@
   runCommand,
   skopeo-nix2container,
   undocker,
+  writeShellApplication,
+  curl,
+  jq,
   go,
+  writers,
   python3Packages,
   cacert,
   ...
@@ -90,23 +94,62 @@ rec {
     '';
 
 
-  pullNeMoImage = image:
-  let
-    variant = lib.findFirst
-      (v: v.architecture == go.GOARCH)
-      (throw "Architecture not supported")
-      image.architectureVariants;
-  in
-    pullImageFromDigest variant.digest {
+  fetchImageDigests =
+    writeShellApplication {
+      name = "fetch-image-digests";
+      text = ''
+        ${lib.getExe curl} \
+          -G https://catalog.ngc.nvidia.com/api/containers/images \
+          -d orgName=nvidia -d name=nemo -d isPublic=true -sSf |
+
+        ${lib.getExe jq} '
+          .images | map({
+            (.tag): .architectureVariants | map({
+              (.architecture): .digest
+            }) | add
+          }) | add
+        '
+      '';
+    };
+
+  pullNeMoImage = tag: digests:
+    pullImageFromDigest digests.${go.GOARCH} {
       registryUrl = "nvcr.io";
       imageName = "nvidia/nemo";
-      imageTag = image.tag;
+      imageTag = tag;
     };
+
+
+  fetchModelHashes =
+    writers.writePython3Bin "fetch-model-hashes" {
+      libraries = with python3Packages; [ huggingface-hub ];
+      flakeIgnore = [ "E111" "E121" "E302" "E305" ];
+    } ''
+      import huggingface_hub as hf
+      from multiprocessing.dummy import Pool
+      import json
+
+      repos = (
+        model.id for model in hf.list_models(
+          author="nvidia", library="nemo", expand="gated"
+        ) if not model.gated
+      )
+
+      def get_name_hash(repo):
+        if hf.file_exists(repo, f"{repo[7:]}.nemo"):
+          url = hf.hf_hub_url(repo, f"{repo[7:]}.nemo")
+          return repo[7:], hf.get_hf_file_metadata(url).etag
+
+      with Pool() as pool:
+        name_hash_pairs = filter(None, pool.map(get_name_hash, repos))
+      print(json.dumps(dict(name_hash_pairs), sort_keys=True, indent=2))
+    '';
 
   pullNeMoModel = name: hash:
     runCommand "${name}.nemo" {
       nativeBuildInputs = [ cacert python3Packages.hf-xet ];
       HF_XET_HIGH_PERFORMANCE = true;
+      outputHashAlgo = "sha256";
       outputHash = hash;
     } ''
       mv $(readlink -e $(HF_HOME=/tmp ${
